11dda5f817dcfdcd28f3264091634f3c6ea6780d
Rob Sturcke <rob@rslabs.dev>
2020-10-10 23:51:19 -0400

    minor mobile fix and better icons

---
+++ src/assets/menu_icon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function MenuIcon(props) {
+  return (
+    <svg
+      version="1.1"
+      className={props.baseLayer}
+      x="0px"
+      y="0px"
+      viewBox="0 0 512 512"
+    >
+      <path d="M64,384h384v-42.7H64V384z M64,277.3h384v-42.7H64V277.3z M64,128v42.7h384V128H64z" />
+    </svg>
+  );
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './logo';
 import { Navbar, Nav } from 'react-bootstrap';
 import Github from '../assets/github';
+import CloseIcon from '../assets/close_icon';
+import MenuIcon from '../assets/menu_icon';
 
-const Header = () => (
-  <header>
-    <Navbar expand="sm">
-      <Navbar.Brand href="#home">
-        <Logo baseLayer="header_logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <a
-            href="https://github.com/robSturcke/brooklyn_pizza_leaflet_ex"
-            className="nav-link"
-          >
-            <Github baseLayer="nav_icon" />
-          </a>
-          <Nav.Link href="#home"></Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </header>
-);
+function Header() {
+  const [onToggle, setToggle] = useState(false);
+  const handleClick = () => setToggle(!onToggle);
+
+  return (
+    <header>
+      <Navbar expand="sm">
+        <Navbar.Brand href="#home">
+          <Logo baseLayer="header_logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleClick}>
+          {onToggle ? (
+            <CloseIcon baseLayer="icon" />
+          ) : (
+            <MenuIcon baseLayer="icon" />
+          )}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <a
+              href="https://github.com/robSturcke/brooklyn_pizza_leaflet_ex"
+              className="nav-link"
+            >
+              <Github baseLayer="nav_icon" />
+            </a>
+            <Nav.Link href="#home"></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+}
 
 export default Header;
